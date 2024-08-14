9a69a781cfdffaab2806a152976a9d62856f3abf
saaismriti <130126070+saaismriti@users.noreply.github.com>
2024-08-14 18:23:26 +0530

    Add files via upload

---
+++ Week 3/Exercise 07/EmployeeManagement/EmployeeManagement/EmployeeManagement/src/main/java/com/library/repository/EmployeeRepository.java
@@ -0,0 +1,44 @@
+package com.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import com.library.entity.Employee;
+
+import java.util.List;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+	
+	Page<Employee> findAll(Pageable pageable);
+	
+
+    List<Employee> findAll(Sort sort);
+	
+    // Find employees by name
+    List<Employee> findByName(String name);
+
+    // Find employees by email
+    @Query("SELECT e FROM Employee e WHERE e.email = :email")
+    Employee findByEmailUsingJPQL(@Param("email") String email);
+
+    // Custom query to find employees by department name using JPQL
+    @Query("SELECT e FROM Employee e WHERE e.department.name = :departmentName")
+    List<Employee> findByDepartmentNameUsingJPQL(@Param("departmentName") String departmentName);
+
+    // Custom query to find employees with a specific name and department using JPQL
+    @Query("SELECT e FROM Employee e WHERE e.name = :name AND e.department.name = :departmentName")
+    List<Employee> findByNameAndDepartment(@Param("name") String name, @Param("departmentName") String departmentName);
+    
+    
+
+    }
+    
+    
+
+
+
