c790fe53eb248d6a4ae447484016b031906fec50
saaismriti <130126070+saaismriti@users.noreply.github.com>
2024-08-14 17:34:27 +0530

    Add files via upload

---
+++ Week 3/Exercise 02/EmployeeManagement/EmployeeManagement/EmployeeManagement/src/main/java/com/library/entity/Department.java
@@ -0,0 +1,45 @@
+package com.library.entity;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+
+    // Getter and Setter methods
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
